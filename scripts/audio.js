b8f1c75a6e2c7af5bc7fb00e4ecb7dca16593ff3
NeVzaimnoePadenie <118021544+AAkismetKLASNI@users.noreply.github.com>
2023-10-06 00:12:54 +0500

    Add files via upload

---
+++ scripts/audio.js
@@ -0,0 +1,124 @@
+function playAudio1() {
+  let audio = document.getElementById("au-1");
+  audio.play();
+}
+
+function playAudio2() {
+  let audio = document.getElementById("au-2");
+  audio.play();
+}
+
+function playAudio3() {
+  let audio = document.getElementById("au-3");
+  audio.play();
+}
+
+function playAudio4() {
+  let audio = document.getElementById("au-4");
+  audio.play();
+}
+
+function playAudio5() {
+  let audio = document.getElementById("au-5");
+  audio.play();
+}
+
+function playAudio6() {
+  let audio = document.getElementById("au-6");
+  audio.play();
+}
+
+function playAudio7() {
+  let audio = document.getElementById("au-7");
+  audio.play();
+}
+
+function playAudio8() {
+  let audio = document.getElementById("au-8");
+  audio.play();
+}
+
+function playAudio9() {
+  let audio = document.getElementById("au-9");
+  audio.play();
+}
+
+function playAudio10() {
+  let audio = document.getElementById("au-10");
+  audio.play();
+}
+
+function playAudio11() {
+  let audio = document.getElementById("au-11");
+  audio.play();
+}
+
+function playAudio12() {
+  let audio = document.getElementById("au-12");
+  audio.play();
+}
+
+function playAudio13() {
+  let audio = document.getElementById("au-13");
+  audio.play();
+}
+
+function playAudio14() {
+  let audio = document.getElementById("au-14");
+  audio.play();
+}
+
+function playAudio15() {
+  let audio = document.getElementById("au-15");
+  audio.play();
+}
+
+function playAudio16() {
+  let audio = document.getElementById("au-16");
+  audio.play();
+}
+
+function playAudio17() {
+  let audio = document.getElementById("au-17");
+  audio.play();
+}
+
+function playAudio18() {
+  let audio = document.getElementById("au-18");
+  audio.play();
+}
+
+function playAudio19() {
+  let audio = document.getElementById("au-19");
+  audio.play();
+}
+
+function playAudio20() {
+  let audio = document.getElementById("au-20");
+  audio.play();
+}
+
+function playAudio21() {
+  let audio = document.getElementById("au-21");
+  audio.play();
+}
+
+function playAudio22() {
+  let audio = document.getElementById("au-22");
+  audio.play();
+}
+
+function playAudio23() {
+  let audio = document.getElementById("au-23");
+  audio.play();
+}
+
+function playAudio24() {
+  let audio = document.getElementById("au-24");
+  audio.play();
+}
+
+function playAudio25() {
+  let audio = document.getElementById("au-25");
+  audio.play();
+}
